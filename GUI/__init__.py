5bc00d1e0d9c4ff652e0fbb2c270aa1d937b86e5
Seweryn <sewerynde@gmail.com>
2019-10-25 21:52:39 +0200

    Componet class created , Labels and entries added

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -1,14 +1,50 @@
-from tkinter import*
+from tkinter import *
 
 
 class Window:
-    def __init__(self,master):
+    def __init__(self, master):
         master.title("HeartRate and SPO2")
         master.geometry("1200x800")
-        self.L = Label(text="Uzupełnij Dane", fg='white', bg='#0B0B0B',anchor='n',width=200, height=100,font=('Courier',44))
-        self.L.pack()
+        master.configure(bg="#0B0B0B")
+
+
+class Components:
+    def __init__(self, master):
+
+        label = Label(master, text="Uzupełnij dane", fg='white', bg="#0B0B0B",
+              font= ('Courier', 44), anchor='center')
+        label.pack(pady=50)
+
+        frame1= Frame(master, bg="#0B0B0B", width="300", height="200")
+        frame1.pack(padx=50)
+
+        frame2 = Frame(master, bg="#0B0B0B", width="300", height="200")
+        frame2.pack(padx=50)
+
+        frame3 = Frame(master, bg="#0B0B0B", width="300", height="200")
+        frame3.pack(padx=50)
+
+        label2 = Label(frame1, text="Imie", fg="white", font=('Courier', 35)
+                       , bg="#0B0B0B", anchor='center')
+        label2.pack()
+
+        label3 = Label(frame2, text="Wiek", fg="white", font=('Courier', 35)
+                       , bg="#0B0B0B", anchor='center')
+        label3.pack()
+
+        label4 = Label(frame3, text="Email", fg="white", font=('Courier', 35)
+                       , bg="#0B0B0B", anchor='center')
+        label4.pack()
+
+        entry1=Entry(frame1, font=('Courier', 35))
+        entry2=Entry(frame2, font=('Courier', 35))
+        entry3=Entry(frame3, font=('Courier', 35))
+        entry1.pack()
+        entry2.pack()
+        entry3.pack()
 
 
 root = Tk()
 Window(root)
+Components(root)
 root.mainloop()
